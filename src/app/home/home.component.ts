f3dce23cd3e2bb3ed23983569ac5f68694d709e6
Annanphy <johnsonanphy@gmail.com>
2025-11-19 16:39:04 +0100

    Updated services section

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  @ViewChild('servicesScroll', { static: false }) servicesScroll!: ElementRef;
 
+  scrollServices(direction: 'left' | 'right') {
+    const container = this.servicesScroll?.nativeElement;
+    if (container) {
+      const scrollAmount = 250;
+      if (direction === 'left') {
+        container.scrollBy({ left: -scrollAmount, behavior: 'smooth' });
+      } else {
+        container.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+      }
+    }
+  }
 }
